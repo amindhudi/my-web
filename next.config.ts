9b31f6c9bfe1a4c69f7551b914faedec4d94aee2
do_muhammad.amin <muhammad.amin@digitalomega.com>
2024-10-29 17:11:38 +0500

    save changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import { withPayload } from "@payloadcms/next/withPayload";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+const nextConfig = {
+  reactStrictMode: true,
+  // other Next.js configuration properties
 };
 
-export default nextConfig;
+export default withPayload(nextConfig);
